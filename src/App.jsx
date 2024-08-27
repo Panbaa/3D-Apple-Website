d1e3c95957e00f15cd0c11a05abcb18f27996224
Gilbert Lynsche <gl@cloudeteer.de>
2024-08-27 15:42:34 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-import {
-  GsapFrom,
-  GsapFromTo,
-  GsapScrollTrigger,
-  GsapStagger,
-  GsapText,
-  GsapTimeline,
-  GsapTo,
-  Home,
-} from "./pages";
 
 const App = () => {
   return (
-    <div className="bg-black min-h-screen w-full">
-      <Router>
-        <Routes>
-          <Route path="/gsapto" element={<GsapTo />} />
-          <Route path="/gsapfrom" element={<GsapFrom />} />
-          <Route path="/gsapfromto" element={<GsapFromTo />} />
-          <Route path="/gsaptimeline" element={<GsapTimeline />} />
-          <Route path="/gsapstagger" element={<GsapStagger />} />
-          <Route path="/gsapscrolltrigger" element={<GsapScrollTrigger />} />
-          <Route path="/gsaptext" element={<GsapText />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </div>
-  );
-};
+    <h1 className='text-3xl font-bold underline'>
+      Hello World!
+    </h1>
+  )
+}
 
-export default App;
+export default App
