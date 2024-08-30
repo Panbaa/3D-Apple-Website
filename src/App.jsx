58a3cd250920aa4af409819b511643b91f43383e
Gilbert Lynsche <gl@cloudeteer.de>
2024-08-30 14:04:06 +0200

    Highlights Navbar Hero done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
+import Navbar from "./components/Navbar"
+import Model from "./components/Model"
 
 const App = () => {
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello World!
-    </h1>
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+    </main>
   )
 }
 
